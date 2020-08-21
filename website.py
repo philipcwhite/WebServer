c8b62f10bc9a13673b8d122aa0904b7ce16a9909
Phil White <philipcwhite@users.noreply.github.com>
2020-08-21 14:54:36 -0400

    Add files via upload

---
+++ website.py
@@ -0,0 +1,21 @@
+from web.server import app, app_vars
+from web.templates import render
+
+class controller(object):
+    def index(self):
+        user = self.get_auth()
+        response = render('index.html', username=user)
+        return response
+
+    def login(self, username=None, password=None):
+        if username is None: return render('login.html')
+        elif username == 'admin' and password == 'test':
+                self.login(username)
+                self.redirect('/')
+        else: return render('error.html')
+
+    def logout(self):
+        self.logout(self.get_user())
+        self.redirect('/login')
+
+app.start(controller)
